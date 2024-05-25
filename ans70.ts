46835cf386da2a37e462aee2dd12afb32876dc88
asadullah48 <asadullahshafique@hotmail.com>
2024-05-25 10:33:30 +0500

    Initial commit

---
+++ ans70.ts
@@ -0,0 +1,20 @@
+// Q 70 Day 24
+// This function prints numbers from 3 to 15 using a loop
+
+function printNumbersWithLet() {
+    for (let i = 1; i <= 5; i++) {
+      console.log(i); // Logs numbers 1 through 5
+    }  
+  };
+  printNumbersWithLet();
+  
+  // Uses let for loop variable i
+  // i is not accessible here, outside the loop, because it's defined with let
+  // This shows that let limits i to the loop, 
+  //preventing errors from using i where it's not intended.
+  function showNumbersWithLet() {
+    for (let a = 12; a <= 15; a++) {
+      console.log(a); 
+    }  
+  };
+  showNumbersWithLet();
